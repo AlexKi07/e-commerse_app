1f11702c57317a635ea5d8dc33af46b630b3a6dd
AlexKi07 <kimanialexk07@gmail.com>
2025-05-28 20:02:06 +0300

    CRUD

---
+++ lib/db/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, S
+from sqlalchemy.orm import relationship, declarative_base
+from datetime import datetime
+
+Base = declarative_base()
+
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    orders = relationship('Order', back_populates='customer', cascade='all, delete-orphan')
